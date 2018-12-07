b1fef11c1e8619c6eac0ee1b4eaa875acc72b19b
makdenis <makdenis.1997@gmail.com>
2018-12-08 01:21:53 +0300

    collisions

--- GameMS/Collision/collision.go
+++ GameMS/Collision/collision.go
@@ -17,7 +17,7 @@ func Collision(xstr string, ystr string, xblockstr string, yblockstr string, x2b
 	if (((x > x2block && x < x2block+60) || (x > x2block+200 && x < x2block+60+200) || (x > x2block+400 && x < x2block+60+400) || (x > x2block+600 && x < x2block+60+600)) && (y < yblock+60+150 && y > yblock+150)) {
 		return true
 	}
-	if (((x > xblock && x < xblock+60) || (x > xblock-200 && x < xblock+60-200) || (x > xblock-400 && x < xblock+60-400) || (x > xblock-600 && x < xblock+60-600)) && (y < yblock+60 && y > yblock)) {
+	if (((x > xblock && x < xblock+60) || (x > xblock-200 && x < xblock+60-200) || (x > xblock-400 && x < xblock+60-400) || (x > xblock-600 && x < xblock+60-600)) && (y < yblock+60-250 && y > yblock-250)) {
 		return true
 	}
 
